83f99a9a6157e2bb96119550b4864f828f8662b9
Ubuntu <ubuntu@ip-172-31-0-21.us-west-1.compute.internal>
2017-03-30 21:26:28 +0000

    adding while_loop.py

---
+++ while_loop.py
@@ -0,0 +1,9 @@
+counter = 50
+
+
+while counter >= 10:
+	print "Counter is still greater than 10"
+	print "Counter is: %s" %(counter)
+	counter = counter - 1
+print "Counter is: %s" %(counter)
+print "Now it's less than 10"
